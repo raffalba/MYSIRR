5be3a694002af7577ddb771e7e58c1a2876c5e6a
Raffaele <raffaele@MacBook-Pro-di-Raffaele-2.local>
2016-04-26 12:36:31 +0200

    MYSIRR v.2.0
    This new version of the tool provides a simple ad easy to use GUI

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-**MY SIRR**
+**MY SIRR v.2.0**
 ----------
 
 ### Minimalist agro-hYdrologicalmodel for Sustainable IRRigation management- soil moisture and crop dynamics*
 ----------
-**MY SIRR** is a set of python scripts that compose a quantitative tool to assess and compare agricultural enterprises across climates, soil types, crops, and irrigation strategies, accounting for the unpredictability of the hydro-climatic forcing.
+**MY SIRR** is a software written in python programming language with a simple Graphical User Interface (GUI) for quantitativly assess and compare agricultural enterprises across climates, soil types, crops, and irrigation strategies, accounting for the unpredictability of the hydro-climatic forcing.
 
 ## Table of Contents
 
@@ -22,7 +22,7 @@
 * [**Acknowledgements**](#acknowledgements)
 
 ## Team
-- [Raffaele Albano] (http://wiki.osgeo.org/wiki/User:Raffalba) [Co-Founder of [Wat-TUBE] (http://wat-tube.it/index.php/it/) spin-off UNIBAS, Research Associate at [University of Basilicata] (http://portale.unibas.it/site/home.html) (UNIBAS), [ECS Representatives] (http://www.egu.eu/ecs/representatives/) of NH Division of European Geosciences Union (EGU)]
+- [Raffaele Albano] (http://www2.unibas.it/raffaelealbano/) [Co-Founder of [Wat-TUBE] (http://wat-tube.it/index.php/it/) spin-off UNIBAS, Research Associate at [University of Basilicata] (http://portale.unibas.it/site/home.html) (UNIBAS), [ECS Representatives] (http://www.egu.eu/ecs/representatives/) of NH Division of European Geosciences Union (EGU)]
 - [Salvatore Manfreda]  (http://www2.unibas.it/manfreda/HydroLab/home.html)[Associate Professor of University of Basilicata UNIBAS, Co-Founder of [Wat-TUBE] (http://wat-tube.it/index.php/it/) spin-off UNIBAS]
 - [Giuseppe Celano](https://sites.google.com/a/agrariaunibas.net/frutticoltura/Home) [Associate Professor of University of Basilicata UNIBAS]
 
@@ -30,12 +30,13 @@
 Learn more about the **MYSIRR** project scope, requirements, and licensing.
 
 ### Scope
-*MY SIRR* is designed to inform farmers, especially from the developing countries or smallholders,for short- and long-term water-related agricultural management. Its parameters are explicit and mostly intuitive and the model maintains sufficient balance between accuracy, simplicity and robustness. The tool accounts in a parsimonious and flexible way for the main climate, soil and vegetation characteristics, and includes the most important source of uncertainty in soil moisture variability (i.e., rainfall) with stochastic approach.
+*MY SIRR* is designed to inform farmers, especially from the developing countries or smallholders, for short- and long-term water-related agricultural management. Its parameters are explicit and mostly intuitive and the model maintains sufficient balance between accuracy, simplicity and robustness. The tool accounts in a parsimonious and flexible way for the main climate, soil and vegetation characteristics, and includes the most important source of uncertainty in soil moisture variability (i.e., rainfall) with stochastic approach.
 
 ### Requirements
 - Requires [Python (v.2.7)] (https://www.python.org/)
 - Requires [matplotlib] (http://matplotlib.org/) and [numpy] (http://www.numpy.org/) package
 - If you use Microsoft excel input file, it is requires also [xlrd] (https://pypi.python.org/pypi/xlrd) and [xlwt] (https://pypi.python.org/pypi/xlwt) python package
+- If you want to use the provided GUI, you should install [PySide] (https://pypi.python.org/pypi/PySide/1.2.4) python package
 
 ### License
 This project is completely licensed [GPL v3+](https://github.com/raffalba/MYSIRR/blob/master/LICENSE).
@@ -51,7 +52,7 @@ This project is completely licensed [GPL v3+](https://github.com/raffalba/MYSIRR
 |      License   |            GNU GPL v.3
 
 ## Quick Start
-The tool is composed by three main classes:.the class that contains the several main functions "[myfun.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/myfun.py)" utilized in the main class "[mysirr.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysirr.py)", that managed the input parameters to evaluate the results and, therefore, to create the outputs, and finally the class "[mysim.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysim.py)" used to read the project file and its components and run the proposed model. *The tool has no user interface. Hence, it can be used in applications where iterative runs are required.*
+The tool is composed by three main classes: the class that contains the several main functions "[myfun.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/myfun.py)" utilized in the main class "[mysirr.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysirr.py)", that managed the input parameters to evaluate the results and, therefore, to create the outputs, and finally the class "[mysim.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysim.py)" used to read the project file and its components and run the proposed model. Finally, the class "[mysirrGUI_mainWindow.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysirrGUI_mainWindow.py)" culd be used to run the model by graphical user interface (GUI). The native file of the GUI is also provided, check "[mainWindow.ui](https://github.com/raffalba/MYSIRR/blob/master/scripts/mainWindow.ui)".
  
 ### Input data
 MY SIRR uses fairly intuitive input variables, either widely used or largely requiring simple methods for their determination. 
@@ -59,7 +60,11 @@ Input data is a project files, encoding in Extensible Markup Language (XLM), com
 <p align="center"><img src="https://github.com/raffalba/MYSIRR/blob/master/img/input.png"/></p>
 
 ### how to run the model
-The tool has no user interface. However, the model simulation could be easily run by terminal or through a batch or python interpreterand. The scripts could be easly used for iterative runs. The user can run the model: (i) lunching the class "[mysim.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysim.py)" that requires as input the project file (XLM) and the name of output file or (ii) lunching the main class "[mysirr.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysirr.py)" giving as input the following information (that are store in xlm format in the project file):
+The model has a simple and easy to use Graphical User Interface (GUI) (see the following figures):
+<p align="center"><img src="https://github.com/raffalba/MYSIRR/blob/master/img/gui.png"/></p>
+The left panel is able to upload the input files,  required to run the model, and to set the path of the output file. The rigth panel can help the users to set the principal parameters, such as information about ET0 (i.e. BT if ET0 is evaluated by Blaney-Criddle method, PM if estimated by Penman-Monteith equation and N if ET0 is provided by the user), set the rain parameters, ie. if it is given by the user (N) or if use stochastic method to evaluate the rainfall time and amount (Y), set the method for the assesment of Yeld (i.e. EMP for empirical formula and DIC for dichotomic  process), and, finally, set the number of cell in which perform the calculation (cell ID). Finally, The "Menu Bar" permits to upload a project file, encoding in Extensible Markup Language (XLM), or to check for recent project files. These files stores the PATH_NAME of all input modules, encoded as comma separated value or Microsoft excel format, required to run the model (see the [Input data](#input) section. In this menu, you can save the project by clicking the button “Save Project”. In case you want to make a copy of the project you can use the button “Save Project as..”.
+
+Moreover you can run the model without the user interface. The model simulation could be easily run by terminal or through a batch or python interpreterand. The scripts could be easly used for iterative runs. The user can run the model: (i) lunching the class "[mysim.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysim.py)" that requires as input the project file (XLM) and the name of output file or (ii) lunching the main class "[mysirr.py](https://github.com/raffalba/MYSIRR/blob/master/scripts/mysirr.py)" giving as input the following information (that are store in xlm format in the project file):
 1) furnish the name of all input modules (i.e. climate.csv, soil.csv, crop.csv and management.csv) 
 2) select the number of the soil cell to run (e.g. 1)
 3) provide information about ET0 (i.e. BT if ET0 is evaluated by Blaney-Criddle method, PM if estimated by Penman-Monteith equation and N if ET0 is provided by the user)
---
+++ scripts/mysirrGUI_mainWindow.py
@@ -0,0 +1,353 @@
+﻿import imp
+try:
+    imp.find_module('xml')
+except ImportError:
+    print(' ERROR IMPORTING LIBRARY   xml')
+try:
+    imp.find_module('PySide')
+except ImportError:
+    print(' ERROR IMPORTING LIBRARY   PySide')
+
+import sys
+import os
+import xml.etree.cElementTree as ET
+from PySide.QtGui import *
+from PySide.QtCore import *
+from ui_mainWindow import Ui_mainWindow
+
+class MainWindow(QMainWindow, Ui_mainWindow):
+    MaxRecentFiles = 5
+    project_name='';
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.recentFileActs = []
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setupUi(self)
+        self.assignWidgets()
+        self.initfield()
+        self.createActions()
+        self.createMenus()
+        self.show()
+    
+    def initfield(self):
+        self.textEditOutput.append("out.xls")
+
+    def assignWidgets(self):
+        self.goButton.clicked.connect(self.goPushed)
+        self.toolButtonClimate.clicked.connect(self.ClimatePushed)
+        self.toolButtonSoil.clicked.connect(self.SoilPushed)
+        self.toolButtonCrop.clicked.connect(self.CropPushed)
+        self.toolButtonMan.clicked.connect(self.ManPushed)
+        self.toolButtonOutput.clicked.connect(self.OutputPushed)
+        QObject.connect(self.actionOpen_Project, SIGNAL("triggered()"), self.open)
+        QObject.connect(self.actionSave_As, SIGNAL("triggered()"), self.saveas)
+        QObject.connect(self.actionSave, SIGNAL("triggered()"), self.save)
+    def loadprojectfile(self, projfile):
+        if os.path.isfile(projfile):
+            tree=ET.parse(projfile)
+            root=tree.getroot()
+            for child in root[0]:
+                a=child.attrib;
+                t=child.text
+                name=a['name']
+                if name.find("climate")>=0:
+                    climate=t;
+     
+                if name.find("soil")>=0:
+                    soil=t
+               
+                if name.find("crop")>=0:
+                    crop=t
+               
+                if name.find("management")>=0:
+                    management=t
+               
+                if name.find("id")>=0:
+                    idcell=t
+                
+                if name.find("ET")>=0:
+                    et=t
+              
+                if name.find("rain")>=0:
+                    rain=t
+                
+                if name.find("vico")>=0:
+                    vico=t
+                
+                if name.find("s_start")>=0:
+                    s_start=t
+
+             #update gui 
+            self.textEditClimate.setPlainText(climate)
+            self.textEditCrop.setPlainText(crop)
+            self.textEditMan.setPlainText(management)
+            self.textEditSoil.setPlainText(soil)
+            if vico=='EMP':
+                id_vico=0
+            else:
+                id_vico=1
+            if rain=='Y':
+                check_rain=True
+            else:
+                check_rain=False
+            if et=='N':
+                id_et=0
+            if et=='BC':
+                id_et=1
+            if et=='PM':
+                id_et=2
+            self.comboBoxVico.setCurrentIndex(id_vico)
+            self.comboBoxET.setCurrentIndex(id_et)
+            self.checkBoxRain.setChecked(check_rain)
+            self.spinBoxCell.setValue(int(idcell))
+            self.doubleSpinBoxSstart.setValue(float(s_start))
+
+            self.setCurrentFile(projfile)
+            self.project_name=projfile
+    def goPushed(self):
+        self.fileClimate=self.textEditClimate.toPlainText()
+        self.fileCrop=self.textEditCrop.toPlainText()
+        self.fileSoil=self.textEditSoil.toPlainText()
+        self.fileMan=self.textEditMan.toPlainText()
+        self.fileout= self.textEditOutput.toPlainText()
+        self.et=self.comboBoxET.currentText()
+        self.vico=self.comboBoxVico.currentText()
+        self.rain=self.checkBoxRain.isChecked()
+        self.s_start=self.doubleSpinBoxSstart.value()
+        self.cellid=self.spinBoxCell.value()
+        self.inputerror=False
+        #check files
+        if os.path.isfile(self.fileClimate)==False:
+            self.goText.append("ERROR: file Climate doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileCrop)==False:
+            self.goText.append("ERROR: file Crop doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileMan)==False:
+            self.goText.append("ERROR: file Management doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileSoil)==False:
+            self.goText.append("ERROR: file Soil doesn't exist ")
+            self.inputerror=True
+        sfile=self.fileout
+        fxls=sfile.find(".xls")
+        fcsv=sfile.find(".csv")
+        if fxls<0 and fcsv<0:
+            self.goText.append("ERROR: file output .xls or .csv  ")
+            self.inputerror=True
+            
+        if self.inputerror==True:
+             p=QMessageBox(self)
+             p.setText("WARNING : SET INPUT FILES")
+             p.show()
+        if self.inputerror==False:
+             self.goText.append("simulation started")
+             if self.rain==True:
+                 rain='Y'
+             else:
+                 rain='N'
+             command='python mysirr.py'
+             command=command+' '+ self.fileClimate +' '+self.fileSoil+' '+self.fileCrop+' '+self.fileMan+' '+str(self.cellid)+' '+self.et+' '+rain+' '+self.fileout+' '+self.vico+' '+str(self.s_start)
+             self.goText.append(command)
+             os.system(command)
+    def ClimatePushed(self):
+        filename=QFileDialog.getOpenFileName(self)
+        if len(filename[0])>0:
+                self.textEditClimate.setPlainText(filename[0])
+    def SoilPushed(self):
+        filename=QFileDialog.getOpenFileName(self)
+        if len(filename[0])>0:
+                self.textEditSoil.setPlainText(filename[0])
+    def CropPushed(self):
+        filename=QFileDialog.getOpenFileName(self)
+        if len(filename[0])>0:
+             self.textEditCrop.setPlainText(filename[0])
+    def ManPushed(self):
+        filename=QFileDialog.getOpenFileName(self)
+        if len(filename[0])>0:
+             self.textEditMan.setPlainText(filename[0])
+    def OutputPushed(self):
+        filename=QFileDialog.getSaveFileName(self)
+        if len(filename[0])>0:
+             self.textEditOutput.setPlainText(filename[0])
+    def open(self):
+         filename=QFileDialog.getOpenFileName(self)
+         if len(filename[0])>0:
+            self.textEditOutput.setPlainText(filename[0])
+    def save(self):
+        self.fileClimate=self.textEditClimate.toPlainText()
+        self.fileCrop=self.textEditCrop.toPlainText()
+        self.fileSoil=self.textEditSoil.toPlainText()
+        self.fileMan=self.textEditMan.toPlainText()
+        self.fileout= self.textEditOutput.toPlainText()
+        self.et=self.comboBoxET.currentText()
+        self.vico=self.comboBoxVico.currentText()
+        self.rain=self.checkBoxRain.isChecked()
+        self.s_start=self.doubleSpinBoxSstart.value()
+        self.cellid=self.spinBoxCell.value()
+        self.inputerror=False
+        #check files
+        if os.path.isfile(self.fileClimate)==False:
+            self.goText.append("ERROR: file Climate doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileCrop)==False:
+            self.goText.append("ERROR: file Crop doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileMan)==False:
+            self.goText.append("ERROR: file Management doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileSoil)==False:
+            self.goText.append("ERROR: file Soil doesn't exist ")
+            self.inputerror=True
+
+        if self.inputerror==False:
+             if self.rain==True:
+                 rain='Y'
+             else:
+                 rain='N'
+             
+                ## write project file
+        if len(self.project_name)>0:
+            outputfileproj=self.project_name;               
+            root=ET.Element("root")
+            doc=ET.SubElement(root,"doc")
+            ET.SubElement(doc,"field1",name="climate").text=self.fileClimate
+            ET.SubElement(doc,"field2",name="management").text=self.fileMan
+            ET.SubElement(doc,"field3",name="soil").text=self.fileSoil
+            ET.SubElement(doc,"field4",name="crop").text=self.fileCrop
+            ET.SubElement(doc,"field5",name="id cell").text=str(self.cellid)
+            ET.SubElement(doc,"field6",name="ET").text=self.et
+            ET.SubElement(doc,"field7",name="rain").text=rain
+            ET.SubElement(doc,"field8",name="vico").text=self.vico
+            ET.SubElement(doc,"field9",name="Tseas").text=str(0)
+            ET.SubElement(doc,"field10",name="s_start").text=str(self.s_start)
+            ET.SubElement(doc,"field11",name="output").text=self.fileout
+            tree=ET.ElementTree(root)
+            tree.write(outputfileproj)
+            self.setCurrentFile(outputfileproj)
+       
+    def saveas(self):
+
+        self.fileClimate=self.textEditClimate.toPlainText()
+        self.fileCrop=self.textEditCrop.toPlainText()
+        self.fileSoil=self.textEditSoil.toPlainText()
+        self.fileMan=self.textEditMan.toPlainText()
+        self.fileout= self.textEditOutput.toPlainText()
+        self.et=self.comboBoxET.currentText()
+        self.vico=self.comboBoxVico.currentText()
+        self.rain=self.checkBoxRain.isChecked()
+        self.s_start=self.doubleSpinBoxSstart.value()
+        self.cellid=self.spinBoxCell.value()
+        self.inputerror=False
+        #check files
+        if os.path.isfile(self.fileClimate)==False:
+            self.goText.append("ERROR: file Climate doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileCrop)==False:
+            self.goText.append("ERROR: file Crop doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileMan)==False:
+            self.goText.append("ERROR: file Management doesn't exist ")
+            self.inputerror=True
+        if os.path.isfile(self.fileSoil)==False:
+            self.goText.append("ERROR: file Soil doesn't exist ")
+            self.inputerror=True
+
+        if self.inputerror==False:
+             if self.rain==True:
+                 rain='Y'
+             else:
+                 rain='N'
+             filename=QFileDialog.getSaveFileName(self)
+             if len(filename[0])>0:
+                 ## write project file
+                outputfileproj=filename[0]                
+                root=ET.Element("root")
+                doc=ET.SubElement(root,"doc")
+                ET.SubElement(doc,"field1",name="climate").text=self.fileClimate
+                ET.SubElement(doc,"field2",name="management").text=self.fileMan
+                ET.SubElement(doc,"field3",name="soil").text=self.fileSoil
+                ET.SubElement(doc,"field4",name="crop").text=self.fileCrop
+                ET.SubElement(doc,"field5",name="id cell").text=str(self.cellid)
+                ET.SubElement(doc,"field6",name="ET").text=self.et
+                ET.SubElement(doc,"field7",name="rain").text=rain
+                ET.SubElement(doc,"field8",name="vico").text=self.vico
+                ET.SubElement(doc,"field9",name="Tseas").text=str(0)
+                ET.SubElement(doc,"field10",name="s_start").text=str(self.s_start)
+                ET.SubElement(doc,"field11",name="output").text=self.fileout
+                tree=ET.ElementTree(root)
+                tree.write(outputfileproj)
+                self.setCurrentFile(outputfileproj)
+                self.project_name=outputfileproj;
+
+    def createActions(self):
+            
+
+            for i in range(MainWindow.MaxRecentFiles):
+                self.recentFileActs.append(
+                        QAction(self, visible=False,
+                                triggered=self.openRecentFile))
+    def openRecentFile(self):
+        action = self.sender()
+        if action:
+            self.loadprojectfile(action.data())
+
+    def createMenus(self):
+        
+        self.separatorAct = self.menuFile.addSeparator()
+        for i in range(MainWindow.MaxRecentFiles):
+            self.action_RecentProject.addAction(self.recentFileActs[i])
+            #self.menuFile.addAction(self.recentFileActs[i])
+        self.menuFile.addSeparator()
+       
+        self.updateRecentFileActions()
+    
+    def updateRecentFileActions(self):
+        settings = QSettings('mysirrGUI', 'Recent Files')
+        files = settings.value('recentFileList')
+
+        files_no = 0
+        if files:
+            files_no = len(files)
+
+        numRecentFiles = min(files_no, MainWindow.MaxRecentFiles)
+
+        for i in range(numRecentFiles):
+            text = "&%d %s" % (i + 1, self.strippedName(files[i]))
+            self.recentFileActs[i].setText(text)
+            self.recentFileActs[i].setData(files[i])
+            self.recentFileActs[i].setVisible(True)
+
+        for j in range(numRecentFiles, MainWindow.MaxRecentFiles):
+            self.recentFileActs[j].setVisible(False)
+
+        self.separatorAct.setVisible((numRecentFiles > 0))
+
+    def strippedName(self, fullFileName):
+        return QFileInfo(fullFileName).fileName()
+
+    def setCurrentFile(self, fileName):
+        self.curFile = fileName
+        
+        settings = QSettings('mysirrGUI', 'Recent Files')
+        files = list(settings.value('recentFileList', []))
+
+        try:
+            files.remove(fileName)
+        except ValueError:
+            pass
+
+        files.insert(0, fileName)
+        del files[MainWindow.MaxRecentFiles:]
+
+        settings.setValue('recentFileList', files)
+
+        for widget in QApplication.topLevelWidgets():
+            if isinstance(widget, MainWindow):
+                widget.updateRecentFileActions()
+      
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    ret = app.exec_()
+    sys.exit( ret )
